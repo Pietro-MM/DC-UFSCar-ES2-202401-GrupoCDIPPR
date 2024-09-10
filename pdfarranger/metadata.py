f07a85a58beba199e3ac3dc3ec83f807b1e64258
Daniel Hiroyuki Watanabe <dahirowata@gmail.com>
2024-09-10 20:20:27 -0300

    metadata add

--- pdfarranger/metadata.py
+++ pdfarranger/metadata.py
@@ -244,7 +244,7 @@ def edit(metadata, pdffiles, parent):
             if row[2] == _MODIFIED:
                 created_date = metadata.get(_CREATED)
                 modified_date = row[1]
-                if created_date and modified_date and modified_date < created_date:
+                if created_date and modified_date and isMinor(modified_date, created_date):
                     msg = _('The modification date cannot be earlier than the creation date.')
                     d = Gtk.MessageDialog(parent=parent,
                                           flags=Gtk.DialogFlags.MODAL,
@@ -257,3 +257,9 @@ def edit(metadata, pdffiles, parent):
 
             metadata[row[2]] = _strtometa(row[1], row[2])
     return r
+
+def compareDates(modified_date, created_date):
+    if modified_date <= created_date:
+        return True
+    else:
+        return False
